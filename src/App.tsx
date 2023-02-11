4d7faee3b71dfeb74d4dce50a58f7ed3df792fda
Julian Lawrance <julian.lawrance@gmail.com>
2023-02-11 00:33:07 +0000

    Add README

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
             <h1>The Grand Hunt Toolkit</h1>
 
             <h4>
-                Some useful tools I created while tackling&nbsp;<a href="https://grandhuntdigital.com/"><em>The Grand Hunt</em></a>&nbsp;with my team <a href="https://grandhuntdigital.com/team/The+Super+Egos"><em>The Super Egos</em></a>.
+                These are some useful tools I created while tackling&nbsp;<a href="https://grandhuntdigital.com/"><em>The Grand Hunt</em></a>&nbsp;with my team <a href="https://grandhuntdigital.com/team/The+Super+Egos"><em>The Super Egos</em></a>.
             </h4>
 
             <Tab menu={{ pointing: true }} panes={panes} />
