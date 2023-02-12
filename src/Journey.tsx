847f5b5b43b38af3ee2c2938af2a7230d438e3b1
Julian L <julian.lawrance@gmail.com>
2023-02-12 18:19:53 +0000

    Add more info to journey details

--- src/Journey.tsx
+++ src/Journey.tsx
@@ -5,23 +5,34 @@ type Map = string[][]
 
 type Position = [number, number]
 
+type JourneyStop = {
+    character: string
+    pos: Position
+    clue: string | null
+}
+
 type JourneyEntry = {
-    // TODO: add starting position
-    // TODO: add list of clues used (if applicable)
     text: string
     map: string
     wrapping: boolean
+    startPos: Position
+    clues: (string | null)[]
 }
 
-const initialClues: string[] = Array(9).fill("")
-const initialMap: Map = Array(5).fill(Array(6).fill(""))
+const MAP_WIDTH = 6
+const MAP_HEIGHT = 5
+const NUM_CLUES = 9
+
+const initialMap: Map = Array(MAP_HEIGHT).fill(Array(MAP_WIDTH).fill(""))
+const initialPos: Position = [2, 2]
+const initialClues: string[] = Array(NUM_CLUES).fill("")
 
 export const Journey = () => {
+    const [map, setMap] = useState(initialMap)
+    const [position, setPosition] = useState<Position>(initialPos)
     const [clues, setClues] = useState(initialClues)
 
-    const [map, setMap] = useState(initialMap)
-    const [position, setPosition] = useState<Position>([0, 0])
-    const [journey, setJourney] = useState("")
+    const [journey, setJourney] = useState<JourneyStop[]>([])
     const [allowWrapping, setAllowWrapping] = useState(false)
 
     const [mapToLoad, setMapToLoad] = useState("")
@@ -29,13 +40,14 @@ export const Journey = () => {
     const [maps, setMaps] = useState<Map[]>([])
 
     const [journeys, setJourneys] = useState<JourneyEntry[]>([])
+    const [journeyDetails, setJourneyDetails] = useState<number>()
 
     const setClue = (clue: string, index: number) => {
         let newClues = clues.map((c, i) => i === index ? clue : c)
         setClues(newClues)
     }
 
-    const extract = (clue: string) => clue.toLowerCase().replace(/[^urdl]/g, "").toUpperCase().split("")
+    const getDirections = (clue: string) => clue.toLowerCase().replace(/[^urdl]/g, "").toUpperCase().split("")
 
     const setData = (value: string, y: number, x: number) => {
         if (value.length > 1) {
@@ -112,18 +124,39 @@ export const Journey = () => {
 
     const left = () => setPosition(moveLeft(position))
 
-    const saveJourney = () => setJourneys([
-        ...journeys,
+    const deleteMap = (index: number) => {
+        let newMaps = [...maps]
+        newMaps.splice(index, 1)
+        setMaps(newMaps)
+    }
+
+    const flattenJourney = (j: JourneyStop[]) => j.flatMap(s => s.character)
+
+    const saveJourney = () => setJourneys(j => [
+        ...j,
         {
-            text: journey,
+            text: flattenJourney(journey).join(""),
             map: flattenMap(map),
             wrapping: allowWrapping,
+            startPos: journey[0].pos,
+            clues: journey.some(s => s.clue != null) ? journey.map(s => s.clue || "") : []
         }
     ])
 
-    const addToJourney = (pos: Position) => {
+    const deleteJourney = (index: number) => {
+        let newJourneys = [...journeys]
+        newJourneys.splice(index, 1)
+        setJourneys(newJourneys)
+    }
+
+    const addToJourney = (pos: Position, clue: string | null) => {
         let newChar = map[pos[1]][pos[0]]
-        setJourney(journey + newChar)
+
+        setJourney(j => [...j, {
+            character: newChar,
+            pos: pos,
+            clue: clue,
+        }])
     }
 
     const nothingToAdd = () => !map[position[1]][position[0]]
@@ -151,9 +184,10 @@ export const Journey = () => {
         setMapToLoad("")
     }
 
-    const apply = (directions: string[]) => {
-        let newPos = position
+    const apply = (pos: Position, clue: string) => {
+        let newPos = pos
 
+        let directions = getDirections(clue)
         for (let dir of directions) {
             switch (dir) {
                 case "U":
@@ -177,8 +211,30 @@ export const Journey = () => {
             }
         }
 
+        let newChar = map[newPos[1]][newPos[0]]
+        let newStop = {
+            character: newChar,
+            pos: newPos,
+            clue: clue,
+        }
+
+        setJourney(j => [...j, newStop])
         setPosition(newPos)
-        addToJourney(newPos)
+
+        return newPos
+    }
+
+    const applyAllAndUpdate = (pos: Position) => {
+        let newPos = pos
+
+        for (let c of clues) {
+            newPos = apply(newPos, c)
+        }
+    }
+
+    const reset = () => {
+        setJourney([])
+        setPosition(initialPos)
     }
 
     const renderButtons = () => <div className="journey-buttons">
@@ -192,7 +248,7 @@ export const Journey = () => {
             <Button
                 color="green"
                 disabled={nothingToAdd()}
-                onClick={() => addToJourney(position)}>
+                onClick={() => addToJourney(position, null)}>
                 Add
             </Button>
 
@@ -225,19 +281,27 @@ export const Journey = () => {
                         </Table.Cell>
 
                         <Table.Cell>
-                            <span>{extract(c).join(", ") || "-"}</span>
+                            <span>{getDirections(c).join(", ") || "-"}</span>
                         </Table.Cell>
 
                         <Table.Cell>
                             <Button
                                 icon
                                 color="green"
-                                onClick={() => apply(extract(c))}>
+                                onClick={() => apply(position, c)}>
                                 <Icon fitted name="play" />
                             </Button>
                         </Table.Cell>
                     </Table.Row>
                 ))}
+
+                <Table.Row>
+                    <Table.Cell colSpan={3}>
+                        <Button fluid color="green" onClick={() => applyAllAndUpdate(position)}>
+                            Apply all clues
+                        </Button>
+                    </Table.Cell>
+                </Table.Row>
             </Table.Body>
         </Table>
     </div>
@@ -252,6 +316,50 @@ export const Journey = () => {
             render: renderClues
         },
     ]
+
+    const renderJourneyDetails = (j: JourneyEntry, index: number) => {
+        let isOpen = journeyDetails === index
+
+        let clues = [...j.clues]
+        for (let i = 0; i < clues.length; i++) {
+            if (!clues[i]) {
+                clues[i] = "(empty)"
+            }
+        }
+
+        return (
+            <div>
+                <div className="journey-header">
+                    <div className="journey-name">
+                        <span><strong>{j.text}</strong></span>
+                    </div>
+
+                    <div className="button-container">
+                        <Button
+                            icon
+                            color="teal"
+                            onClick={() => setJourneyDetails(isOpen ? undefined : index)}>
+                            <Icon fitted name={isOpen ? "chevron up" : "chevron down"} />
+                        </Button>
+
+                        <Button
+                            icon
+                            color="red"
+                            onClick={() => deleteJourney(index)}>
+                            <Icon fitted name="trash" />
+                        </Button>
+                    </div>
+                </div>
+
+                {isOpen && <div className="journey-details">
+                    <div>Map: <em>{j.map}</em></div>
+                    <div>Wrapping: <em>{j.wrapping ? "on" : "off"}</em></div>
+                    <div>Start position: <em>({j.startPos[0]}, {j.startPos[1]})</em></div>
+                    <div>Clues: <em>{clues.length > 0 ? clues.join(", ") : "(none)"}</em></div>
+                </div>}
+            </div>
+        )
+    }
 
     return (
         <div className="columns">
@@ -269,7 +377,7 @@ export const Journey = () => {
             <div className="container">
                 <div className="journey-string-container">
                     <div className="journey-string">
-                        <p>{journey || "?"}</p>
+                        <p>{flattenJourney(journey).join("") || "?"}</p>
                     </div>
 
                     <ButtonGroup>
@@ -283,7 +391,7 @@ export const Journey = () => {
                         <Button
                             color="red"
                             disabled={!journey}
-                            onClick={() => setJourney("")}>
+                            onClick={reset}>
                             Reset
                         </Button>
                     </ButtonGroup>
@@ -332,30 +440,41 @@ export const Journey = () => {
                         <Table.Header>
                             <Table.Row>
                                 <Table.HeaderCell>Maps</Table.HeaderCell>
-                                <Table.HeaderCell>Copy</Table.HeaderCell>
+                                <Table.HeaderCell></Table.HeaderCell>
                             </Table.Row>
                         </Table.Header>
 
                         <Table.Body>
                             {maps.length <= 0 && <Table.Row>
-                                <Table.Cell className="placeholder-cell" colSpan={2}>
+                                <Table.Cell className="placeholder-cell">
                                     None yet!
                                 </Table.Cell>
                             </Table.Row>}
 
-                            {maps.map(m => (
+                            {maps.map((m, i) => (
                                 <Table.Row>
                                     <Table.Cell>
-                                        {m.flatMap(r => r).join("")}
+                                        <div className="map-name">
+                                            <span><strong>{m.flatMap(r => r).join("")}</strong></span>
+                                        </div>
                                     </Table.Cell>
 
                                     <Table.Cell>
-                                        <Button
-                                            icon
-                                            color="yellow"
-                                            onClick={() => navigator.clipboard.writeText(m.flatMap(r => r).join(""))}>
-                                            <Icon fitted name="copy" />
-                                        </Button>
+                                        <div className="button-container">
+                                            <Button
+                                                icon
+                                                color="yellow"
+                                                onClick={() => navigator.clipboard.writeText(m.flatMap(r => r).join(""))}>
+                                                <Icon fitted name="copy" />
+                                            </Button>
+
+                                            <Button
+                                                icon
+                                                color="red"
+                                                onClick={() => deleteMap(i)}>
+                                                <Icon fitted name="trash" />
+                                            </Button>
+                                        </div>
                                     </Table.Cell>
                                 </Table.Row>
                             ))}
@@ -378,12 +497,10 @@ export const Journey = () => {
                                 </Table.Cell>
                             </Table.Row>}
 
-                            {journeys.map(g => (
+                            {journeys.map((j, i) => (
                                 <Table.Row>
                                     <Table.Cell>
-                                        <div><strong>{g.text}</strong></div>
-                                        <div><em>Map: {g.map}</em></div>
-                                        <div><em>Wrapping {g.wrapping ? "ON" : "OFF"}</em></div>
+                                        {renderJourneyDetails(j, i)}
                                     </Table.Cell>
                                 </Table.Row>
                             ))}
