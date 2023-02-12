ad295344063f417d3574f19814166a2029299970
Julian L <julian.lawrance@gmail.com>
2023-02-12 10:55:41 +0000

    Add interface for inputting clue answers, which reveals Journey directions

--- src/Journey.tsx
+++ src/Journey.tsx
@@ -1,13 +1,24 @@
 import { useState } from "react"
-import { Button, Checkbox, Input, Segment } from "semantic-ui-react"
+import { Button, ButtonGroup, Checkbox, Icon, Input, Segment, Table } from "semantic-ui-react"
 
-const initialData: string[][] = Array(5).fill(Array(6).fill(""))
+const initialClues: string[] = Array(9).fill("")
+const initialCellData: string[][] = Array(5).fill(Array(6).fill(""))
 
 export const Journey = () => {
-    const [cellData, setCellData] = useState<string[][]>(initialData)
+    const [clues, setClues] = useState(initialClues)
+    const [clueCount, setClueCount] = useState(9)
+
+    const [cellData, setCellData] = useState(initialCellData)
     const [position, setPosition] = useState<[number, number]>([0, 0])
     const [journey, setJourney] = useState("")
     const [allowWrapping, setAllowWrapping] = useState(false)
+
+    const setClue = (clue: string, index: number) => {
+        let newClues = clues.map((c, i) => i === index ? clue : c)
+        setClues(newClues)
+    }
+
+    const extract = (clue: string) => clue.toLowerCase().replace(/[^urdl]/g, "").toUpperCase().split("")
 
     const setData = (value: string, y: number, x: number) => {
         if (value.length > 1) {
@@ -88,51 +99,101 @@ export const Journey = () => {
     const nothingToAdd = () => !cellData[position[1]][position[0]]
 
     return (
-        <div className="container">
-            <div className="journey-string-container">
-                <div className="journey-string">
-                    <p>{journey || "?"}</p>
+        <div className="columns">
+            <div className="clues">
+                <Table>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>Clue</Table.HeaderCell>
+                            <Table.HeaderCell>Directions</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+
+                    <Table.Body>
+                        {clues.slice(0, clueCount).map((c, i) => (
+                            <Table.Row>
+                                <Table.Cell>
+                                    <Input
+                                        placeholder={"Answer " + (i + 1)}
+                                        value={c}
+                                        onChange={(e, data) => setClue(data.value as string, i)} />
+                                </Table.Cell>
+
+                                <Table.Cell>
+                                    <span>{extract(c).join(", ") || "?"}</span>
+                                </Table.Cell>
+                            </Table.Row>
+                        ))}
+
+                        <Table.Row>
+                            <Table.Cell colSpan={2}>
+                                <ButtonGroup fluid>
+                                    <Button
+                                        icon
+                                        disabled={clueCount >= 9}
+                                        onClick={() => setClueCount(clueCount + 1)}>
+                                        <Icon fitted name="add" />
+                                    </Button>
+
+                                    <Button
+                                        icon
+                                        disabled={clueCount <= 1}
+                                        onClick={() => setClueCount(clueCount - 1)}>
+                                        <Icon fitted name="minus" />
+                                    </Button>
+                                </ButtonGroup>
+                            </Table.Cell>
+                        </Table.Row>
+                    </Table.Body>
+                </Table>
+            </div>
+
+            <div className="container">
+                <div className="journey-string-container">
+                    <div className="journey-string">
+                        <p>{journey || "?"}</p>
+                    </div>
+
+                    <Button
+                        color="red"
+                        disabled={!journey}
+                        onClick={() => setJourney("")}>
+                        Reset
+                    </Button>
                 </div>
 
-                <Button
-                    color="red"
-                    disabled={!journey}
-                    onClick={() => setJourney("")}>
-                    Reset
-                </Button>
+                <div className="grid">
+                    {cellData.map(renderRow)}
+                </div>
+
+                <div className="journey-buttons">
+                    <Button onClick={up}>Up</Button>
+                </div>
+
+                <div className="journey-buttons">
+                    <Button onClick={left}>Left</Button>
+
+                    <Button
+                        color="green"
+                        disabled={nothingToAdd()}
+                        onClick={addToJourney}>
+                        Add
+                    </Button>
+
+                    <Button onClick={right}>Right</Button>
+                </div>
+
+                <div className="journey-buttons">
+                    <Button onClick={down}>Down</Button>
+                </div>
+
+                <Segment>
+                    <Checkbox
+                        label="Allow wrapping"
+                        checked={allowWrapping}
+                        onChange={(e, data) => setAllowWrapping(data.checked || false)} />
+                </Segment>
             </div>
-
-            <div className="grid">
-                {cellData.map(renderRow)}
-            </div>
-
-            <div className="journey-buttons">
-                <Button onClick={up}>Up</Button>
-            </div>
-
-            <div className="journey-buttons">
-                <Button onClick={left}>Left</Button>
-
-                <Button
-                    color="green"
-                    disabled={nothingToAdd()}
-                    onClick={addToJourney}>
-                    Add
-                </Button>
-
-                <Button onClick={right}>Right</Button>
-            </div>
-
-            <div className="journey-buttons">
-                <Button onClick={down}>Down</Button>
-            </div>
-
-            <Segment>
-                <Checkbox
-                    label="Allow wrapping"
-                    checked={allowWrapping}
-                    onChange={(e, data) => setAllowWrapping(data.checked || false)} />
-            </Segment>
         </div>
     )
 }
